cd30ee9e0774c254c69e2b068f7328ea479cf81e
Nehir Serin <1nhrserin@gmail.com>
2025-02-19 12:47:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,27 +5,29 @@ import matplotlib.pyplot as plt
 
 path='best.pt'
 
+model = torch.hub.load(
+    'WongKinYiu/yolov7', 
+    'custom', 
+    path,
+    force_reload=True,
+    trust_repo=True)
+
 def get_yolov7_annotations(img_path):
-    model = torch.hub.load(
-        'WongKinYiu/yolov7', 
-        'custom', 
-        path,
-        force_reload=True,
-        trust_repo=True)
-   
-    img = cv2.imread(img_path)
+img = cv2.imread(img_path)
     results = model(img)
-    boxes = results.xywh[0].cpu().numpy()  # xywh format (x_center, y_center, width, height)
+    boxes = results.xyxy[0]
     if len(boxes) == 0:
         print("No objects detected.")
         return None
+    boxes = boxes.cpu().numpy()
     coordinates = []
     for box in boxes:
-        x_center, y_center, width, height, conf, cls = box
-        x1 = int((x_center - width / 2))  # top-left x
-        y1 = int((y_center - height / 2))  # top-left y
-        x2 = int((x_center + width / 2))  # bottom-right x
-        y2 = int((y_center + height / 2))  # bottom-right y
+        x1, y1, x2, y2, conf, cls = box
+        x1 = int(x1)  # top-left x,
+        y1 = int(y1)
+        x2 = int(x2)
+        y2 = int(y2)
+
         coordinates.append((x1, y1, x2, y2))  # append the coordinates
         cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)  # Red color, 2 thickness
         
